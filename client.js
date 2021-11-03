63c7f4cb6705596c7fe93ed588db50808165f39d
Aaron K <tonberryspawn@gmail.com>
2021-11-03 13:09:51 -0700

    adding functions

--- client.js
+++ client.js
@@ -1,20 +1,29 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here, ex. 10.0.0.209 or 'localhost'
+    host: '165.227.47.243', // IP address here, ex. '10.0.0.209' or 'localhost'
     port: 50541 // PORT number here
   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+  conn.setEncoding("utf8"); // interpret incoming data as text
 
   conn.on('data', (data) => {
-    console.log('Server message: ', data);
+    console.log('Server message:', data);
   });
 
+  conn.on('connect', () => {
+  console.log('Successfully connected to game server');
+  conn.write('Name: AAR');
+  });
+  
   return conn;
 };
 
-module.exports = { connect };
+console.log("Connecting ...");
+connect();
+
+module.exports = {
+  connect // ES6 shorthand syntax for 'connect': connect
+};
--- play.js
+++ play.js
@@ -1,14 +1,42 @@
 const net = require("net");
-const { connect } = require('./client');
+const { connect } = require('./client'); // ES6 shorthand syntax to destructure object
 
-console.log("Connecting ...");
-connect();
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+// Event listener for stdin
+const handleUserInput = function(key) {
+  // your code here
+  if (key === '\u0003') { // Ctrl+C input to terminate
+    console.log('Exiting...');
+    process.exit();
+  }
+};
+
+setupInput();
+
+
+// conn.on('connect', () => {
+//   console.log('Successfully connected to game server');
+//   conn.write('Name: AAR');
+//   // setTimeout(() => {
+//   //   conn.write('Move: up');
+//   // }, 1000);
+// });
+
+
+
+
+
+
+
 
 
 // can terminate app with Ctrl+C
-
-// // event handling
-// conn.on("event name", functionThatDoesSomething);
-// conn.on("event name", () => {
-//   // code that does something
-// });
